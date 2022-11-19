56f92c4b7092e46d302d20277fc6c07fb9566255
dongkyunkim <49021557+dongkyunkimdev@users.noreply.github.com>
2022-11-20 05:09:27 +0900

    Time: 214 ms (98.02%), Space: 35.5 MB (99.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -2,16 +2,14 @@ class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
         val charMap = hashMapOf<Char, Int>()
         var leftIdx = 0
-        var rightIdx = 0
         var maxLength = 0
 
-        while (rightIdx < s.length) {
-            if (charMap.containsKey(s[rightIdx]) && charMap[s[rightIdx]]!! >= leftIdx) {
-                leftIdx = charMap[s[rightIdx]]!! + 1
+        s.forEachIndexed { rightIdx, currentChar ->
+            charMap[currentChar]?.takeIf { it >= leftIdx }?.let {
+                leftIdx = it + 1
             }
-            charMap[s[rightIdx]] = rightIdx
-            maxLength = maxLength.coerceAtLeast(rightIdx - leftIdx + 1)
-            rightIdx++
+            maxLength = kotlin.math.max(maxLength, rightIdx - leftIdx + 1)
+            charMap[currentChar] = rightIdx
         }
 
         return maxLength
