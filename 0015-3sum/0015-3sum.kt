5de8bbaf5213b9e8d10d36ce9ec3cc3f3f798180
dongkyunkim <49021557+dongkyunkimdev@users.noreply.github.com>
2022-11-24 23:46:07 +0900

    Time: 1686 ms (47.86%), Space: 85 MB (46.00%) - LeetHub

--- 0015-3sum/0015-3sum.kt
+++ 0015-3sum/0015-3sum.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> {
-        val resultList = arrayListOf<List<Int>>()
+        val resultSet = hashSetOf<List<Int>>()
         val usedNum = hashMapOf<Int, Boolean>()
         var leftIdx = 1
         var rightIdx = nums.size - 1
@@ -9,31 +9,25 @@ class Solution {
             if (usedNum[sortedNums[i]] == null) {
                 leftIdx = i + 1
                 rightIdx = nums.size - 1
-                var lastLeftValue = sortedNums[i] - 1
-                var lastRightValue = sortedNums[rightIdx] + 1
                 while (leftIdx < rightIdx) {
                     if (sortedNums[i] < 0 && sortedNums[rightIdx] <= 0) break;
                     else if (sortedNums[i] == 0 && sortedNums[leftIdx] > 0) break;
                     else if (sortedNums[i] > 0) break;
-
-                    if (sortedNums[i] + sortedNums[leftIdx] + sortedNums[rightIdx] > 0) {
-                        rightIdx--
-                    } else if (sortedNums[i] + sortedNums[leftIdx] + sortedNums[rightIdx] < 0) {
-                        leftIdx++
-                    } else if (lastLeftValue == sortedNums[leftIdx]) {
-                        leftIdx++
-                    } else if (lastRightValue == sortedNums[rightIdx]) {
-                        rightIdx--
-                    } else {
-                        resultList.add(listOf(sortedNums[i], sortedNums[leftIdx], sortedNums[rightIdx]))
-                        lastLeftValue = sortedNums[leftIdx]
-                        lastRightValue = sortedNums[rightIdx]
+                    val sum = sortedNums[i] + sortedNums[leftIdx] + sortedNums[rightIdx]
+                    when {
+                        sum > 0 -> rightIdx--
+                        sum < 0 -> leftIdx++
+                        else -> {
+                            resultSet.add(listOf(sortedNums[i], sortedNums[leftIdx], sortedNums[rightIdx]))
+                            leftIdx++
+                            rightIdx--
+                        }
                     }
                 }
                 usedNum[sortedNums[i]] = true
             }
         }
 
-        return resultList
+        return resultSet.toList()
     }
 }
