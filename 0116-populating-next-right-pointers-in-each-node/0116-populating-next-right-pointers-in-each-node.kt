c58695137e685b64be940c7c4bb009982cf64537
dongkyunkim <49021557+dongkyunkimdev@users.noreply.github.com>
2022-12-08 23:58:58 +0900

    Time: 504 ms (5.47%), Space: 46.1 MB (14.84%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.kt
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 /**
  * Definition for a Node.
  * class Node(var `val`: Int) {
@@ -9,29 +11,26 @@
 
 class Solution {
     fun connect(root: Node?): Node? {
-        bfs(root)
-        return root
-        
-    }
-    
-    fun bfs(root: Node?) {
-        if (root == null) return
-        val q = LinkedList<Node>()
-        q.add(root!!)
-        while(!q.isEmpty()) {
-            val size = q.size-1
-            for (i in 0..size) {
-                val currentNode = q.poll()
-                if (i != size) {
-                    currentNode.next = q.peek()
+        if (root == null) return null
+        val queue = ArrayDeque<Node>()
+        queue.offer(root)
+
+        while (queue.isNotEmpty()) {
+            val queueSize = queue.size
+            for (i in 0 until queueSize) {
+                val currentNode = queue.poll()
+                if (i + 1 < queueSize) {
+                    currentNode.next = queue.peek()
                 }
-                currentNode.left?.let {
-                    q.add(it)
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left)
                 }
-                currentNode.right?.let {
-                    q.add(it)
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right)
                 }
             }
         }
+
+        return root
     }
 }
