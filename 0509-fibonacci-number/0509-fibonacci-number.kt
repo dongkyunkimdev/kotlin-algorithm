bcf3c6809be544af6fef435bb4451352cafe5c33
dongkyunkim <ddkds66@gmail.com>
2022-12-22 01:58:34 +0900

    Time: 424 ms (5.25%), Space: 100.7 MB (5.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.kt
+++ 0509-fibonacci-number/0509-fibonacci-number.kt
@@ -1,6 +1,15 @@
 class Solution {
     fun fib(n: Int): Int {
-        return if (n <= 1) n
-        else fib(n - 1) + fib(n - 2)
+        if (n <= 1) return n
+        var result = 0
+        val array = arrayOf(0, 1)
+        if (n < array.size) {
+            result = array[n]
+        } else {
+            result = fib(n - 1) + fib(n - 2)
+            array.plus(result)
+        }
+
+        return result
     }
 }
